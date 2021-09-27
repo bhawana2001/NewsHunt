57a6cc5321c4607f4d65de737ddeb101be75ce2d
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-27 18:23:10 +0530

    change some props

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,15 +4,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
    
     render() {
-       let  {title,description,imageurl,newsurl}=this.props;
+       let  {title,description,imageUrl,newsUrl}=this.props;
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageurl} className="card-img-top" alt="..."/>
+                    <img src={imageUrl} className="card-img-top" alt="..."/>
                     <div class ="card-body">
-                    <h5 class ="card-title">{title}</h5>
-                    <p class ="card-text">{description}</p>
-                    <a href="/newsdetail/" class ="btn btn-sm btn-primary">Read More</a>
+                    <h5 class ="card-title">{title}...</h5>
+                    <p class ="card-text">{description}...</p>
+                    <a href={newsUrl} target="_blank" class ="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
