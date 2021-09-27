64d68a4846e2a22856e6eb80f3d57e1f1381005d
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-27 17:28:57 +0530

    add sample news data

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,20 @@
+import { getByTitle } from '@testing-library/dom'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
+       let  {title,description}=this.props;
         return (
             <div>
-                This is a news item ....
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/16EE0/production/_120702939_whatsubject.jpg" className="card-img-top" alt="..."/>
+                    <div class ="card-body">
+                    <h5 class ="card-title">{title}</h5>
+                    <p class ="card-text">{description}</p>
+                    <a href="/newsdetail" class ="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
         )
     }
