ef32e9ce9fed1e310a7384c35b87c2a94efe353e
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-27 17:57:50 +0530

    add this.props

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,17 @@ import { getByTitle } from '@testing-library/dom'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-
+   
     render() {
-       let  {title,description}=this.props;
+       let  {title,description,imageurl,newsurl}=this.props;
         return (
-            <div>
+            <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/16EE0/production/_120702939_whatsubject.jpg" className="card-img-top" alt="..."/>
+                    <img src={imageurl} className="card-img-top" alt="..."/>
                     <div class ="card-body">
                     <h5 class ="card-title">{title}</h5>
                     <p class ="card-text">{description}</p>
-                    <a href="/newsdetail" class ="btn btn-primary">Go somewhere</a>
+                    <a href="/newsdetail/" class ="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
