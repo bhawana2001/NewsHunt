aef282a254f8b39ee1a30126108a807f615adaed
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-27 17:56:44 +0530

    add constructor

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,62 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    articles = [
+        {
+            "source": {
+                "id": "bbc-sport",
+                "name": "BBC Sport"
+            },
+            "author": "BBC Sport",
+            "title": "Moeen retires from Test cricket",
+            "description": "England all-rounder Moeen Ali announces his retirement from Test cricket.",
+            "url": "http://www.bbc.co.uk/sport/cricket/58701999",
+            "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/16EE0/production/_120702939_whatsubject.jpg",
+            "publishedAt": "2021-09-27T07:22:27.1010183Z",
+            "content": "Moeen Ali took the third most Test wickets by a spinner for England\r\nEngland all-rounder Moeen Ali has announced his retirement from Test cricket, but will continue to play in limited-overs internati… [+330 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ]
+    constructor() {
+        super();
+        this.state = {
+               articles: this.articles,
+               loading:false
+        }
+    }
+
     render() {
         return (
             <div className="container my-3">
                 <h2>NewsHunt - Top Headlines </h2>
                 <div className="row">
                     <div className="col md-4">
-                        <NewsItem title="myTitle" description="mydesc" />
+                        <NewsItem title="myTitle" description="mydesc" imageurl="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/16EE0/production/_120702939_whatsubject.jpg" newsurl="todo"/>
                     </div>
                     <div className="col md-3">
                         <NewsItem title="myTitle" description="mydesc" />
@@ -17,7 +66,7 @@ export class News extends Component {
                         <NewsItem title="myTitle" description="mydesc" />
                     </div>
 
-                    
+
                 </div>
 
             </div>
