790341a5d54b6873f94de45c609ee2a33d147f1e
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-28 09:41:12 +0530

    make all buttons dark

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,17 @@ import { getByTitle } from '@testing-library/dom'
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-   
+
     render() {
-       let  {title,description,imageUrl,newsUrl}=this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
-                    <div class ="card-body">
-                    <h5 class ="card-title">{title}...</h5>
-                    <p class ="card-text">{description}...</p>
-                    <a href={newsUrl} target="_blank" class ="btn btn-sm btn-primary">Read More</a>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl ? imageUrl : "https://www.xda-developers.com/files/2021/09/iPhone-13-Pro-Mini-Max-XDA-23424lkjkljsdf243.jpeg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
